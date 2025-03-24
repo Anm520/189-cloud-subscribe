4c8a374e32b6534fea47b343c4e7a0f46360fabd
Anm520 <944606394@qq.com>
2025-03-24 21:21:34 +0800

    前端包fixbug

---
+++ cloud-vue-h5/assets/config-DSbuVonj.js
@@ -0,0 +1,1 @@
+import{d as o,r as t}from"./index-CS16L17F.js";const a=o("config",()=>{const e=t("");function r(n){e.value=n}return{chanel_url:e,setChangelUrl:r}});export{a as u};
--- cloud-vue-h5/assets/config-Ddli9dty.js
+++
@@ -1,1 +0,0 @@
-import{d as o,r as t}from"./index-C2vdb2pt.js";const a=o("config",()=>{const e=t("");function r(n){e.value=n}return{chanel_url:e,setChangelUrl:r}});export{a as u};
